b7cbdf3cdc7482a9d2e84f463a39d5719922cba2
MrVizious <mrvizious@gmail.com>
2023-07-25 04:17:26 +0200

    DOTween and fixed anchored scales for Images in Fail Points

--- Assets/_Home_/Scripts/UI/FailCounter.cs
+++ Assets/_Home_/Scripts/UI/FailCounter.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using ExtensionMethods;
+using Sirenix.OdinInspector;
 
 public class FailCounter : MonoBehaviour
 {
     public int maxFailPoints = 10;
     public Image stolenItemsImage, unsatisfiedClientsImage, trashPointsImage;
-    private int _stolenItems;
-    private int stolenItems
+    [SerializeField]
+    [OnValueChanged("UpdateGraphics")]
+    private int _stolenItems = 0;
+    public int stolenItems
     {
         get => _stolenItems;
         set
@@ -17,8 +21,10 @@ public class FailCounter : MonoBehaviour
             _stolenItems = value;
         }
     }
-    private int _unsatisfiedClients;
-    private int unsatisfiedClients
+    [SerializeField]
+    [OnValueChanged("UpdateGraphics")]
+    private int _unsatisfiedClients = 0;
+    public int unsatisfiedClients
     {
         get => _unsatisfiedClients;
         set
@@ -27,8 +33,10 @@ public class FailCounter : MonoBehaviour
             _unsatisfiedClients = value;
         }
     }
-    private int _trashPoints;
-    private int trashPoints
+    [SerializeField]
+    [OnValueChanged("UpdateGraphics")]
+    private float _trashPoints = 0;
+    public float trashPoints
     {
         get => _trashPoints;
         set
@@ -38,16 +46,23 @@ public class FailCounter : MonoBehaviour
         }
     }
 
-    public void AddStolenItem()
+    [Button]
+    public void UpdateGraphics()
     {
+        // Stolen items
+        stolenItemsImage.rectTransform.localScale = new Vector2(stolenItems, 1f);
 
+        // Unsatisfied clients
+        unsatisfiedClientsImage.rectTransform.anchoredPosition = new Vector2(stolenItems + 0.5f, 0f);
+        unsatisfiedClientsImage.rectTransform.localScale = new Vector2(unsatisfiedClients, 1f);
+
+        // Trash points
+        trashPointsImage.rectTransform.anchoredPosition = new Vector2(stolenItems + unsatisfiedClients + 0.5f, 0f);
+        trashPointsImage.rectTransform.localScale = new Vector2(trashPoints, 1f);
     }
-    public void AddUnsatisfiedClient()
-    {
 
-    }
-    public void AddTrashPoint()
+    private void Start()
     {
-
+        UpdateGraphics();
     }
 }
